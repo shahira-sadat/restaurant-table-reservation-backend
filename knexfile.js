14173d40c872cfc7231bada97bff1f196f092e4e
shahira-sadat <shahira.sadat1@gmail.com>
2023-03-19 23:26:37 +0430

    Add settings

---
+++ knexfile.js
@@ -0,0 +1,41 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgres",
+    connection: {
+      database: "tablereserve",
+    },
+  },
+
+  staging: {
+    client: "postgres",
+    connection: {
+      database: "tablereserve",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgres",
+    connection: {
+      database: "tablereserve-production",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
